fc522fe2648331aa8cbe27e0ef3c236985856692
DEBJIT PUROHIT <92798917+debjitpurohit@users.noreply.github.com>
2024-02-08 11:37:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      // watch: {
+      //   usePolling: true,
+      // },
+      //host: true, // needed for the Docker Container port mapping to work
+      origin: process.env.VITE_SERVER_URL,
+      proxy: {
+        '^/api/.*': {
+          target: process.env.VITE_SERVER_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  });
+};
